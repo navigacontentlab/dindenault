03798db94a9d84f654f102ef3345a3406be0bfaa
Fredrik Gustafsson <fredrik.gustafsson@navigacontentlab.com>
2025-09-17 15:30:01 +0200

    removed xray and updated telemetry

--- navigaid/context.go
+++ navigaid/context.go
@@ -3,6 +3,9 @@ package navigaid
 import (
 	"context"
 	"errors"
+
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/trace"
 )
 
 type contextKey int
@@ -43,13 +46,20 @@ func SetAuth(ctx context.Context, auth AuthInfo, err error) context.Context {
 	})
 }
 
-// AddAnnotation adds an annotation to the XRay segment in the current context.
+// AddAnnotation adds an annotation to the current tracing span in the context.
 func AddAnnotation(ctx context.Context, key string, value string) {
-	// Add XRay annotation if available
-	addXRayAnnotation(ctx, key, value)
+	addOpenTelemetryAnnotation(ctx, key, value)
 }
 
-// AddUserAnnotation adds a user annotation to the XRay segment in the current context.
+// AddUserAnnotation adds a user annotation to the current tracing span in the context.
 func AddUserAnnotation(ctx context.Context, user string) {
 	AddAnnotation(ctx, "user", user)
+}
+
+// addOpenTelemetryAnnotation adds an annotation to the current OpenTelemetry span.
+func addOpenTelemetryAnnotation(ctx context.Context, key string, value string) {
+	span := trace.SpanFromContext(ctx)
+	if span.IsRecording() {
+		span.SetAttributes(attribute.String(key, value))
+	}
 }
--- navigaid/go.mod
+++ navigaid/go.mod
@@ -4,25 +4,14 @@ go 1.23.6
 
 require (
 	connectrpc.com/connect v1.18.1
-	github.com/aws/aws-xray-sdk-go v1.8.5
 	github.com/golang-jwt/jwt/v4 v4.5.2
+	go.opentelemetry.io/otel v1.35.0
+	go.opentelemetry.io/otel/trace v1.35.0
 )
 
 require (
-	github.com/andybalholm/brotli v1.1.1 // indirect
-	github.com/aws/aws-sdk-go v1.55.7 // indirect
-	github.com/jmespath/go-jmespath v0.4.0 // indirect
-	github.com/klauspost/compress v1.18.0 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
-	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasthttp v1.61.0 // indirect
 	golang.org/x/net v0.39.0 // indirect
-	golang.org/x/sys v0.32.0 // indirect
-	golang.org/x/text v0.24.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20250428153025-10db94c68c34 // indirect
-	google.golang.org/grpc v1.72.0 // indirect
 	google.golang.org/protobuf v1.36.6 // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
 
 replace github.com/navigacontentlab/dindenault => ../
